30f499b1617764f5eef46cbf528ae6ea14877cf7
Teodoro PICCINNI <studio@teodoropiccinni.com>
2019-07-08 21:22:42 +0200

    Pushing Plug-In core
    Sul branch dev
    Changes to be committed:
    	new file:   instagram-embedded-custom.php
    	new file:   readme.txt

---
+++ instagram-embedded-custom.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package Instagram_Embedded_Custom
+ * @version 0.0.1
+ */
+/*
+Plugin Name: Instagram Embedded Custom
+Plugin URI: http://wordpress.org/plugins/instagram-embedded-custom/
+Description: This plugin will help you in removing some frustrating description and comments from embedded Instagram photos and videos.
+Author: Teodoro PICCINNI
+Version: 0.0.1
+Author URI: https://www.teodoropiccinni.com/
+*/
+
+// Process Instagram embedded contents
+function instagram_embedded_customizer($content) {
+	/** Customize post content from plugin settings */
+	if(strpos($content, 'instagr.am') !== false || strpos($content, 'instagram.com') !== false){ // if instagram embed
+	    $return = preg_replace("@data-instgrm-captioned@", "", $content); // remove caption class
+	    return $return;		
+    }
+	return $content;
+}
+
+
+// Now we set that function up to execute when the embed_handler_html and embed_oembed_html action is called.
+add_filter('embed_handler_html', 'instagram_embedded_customizer');
+add_filter('embed_oembed_html', 'instagram_embedded_customizer');
+
---
+++ readme.txt
@@ -0,0 +1,11 @@
+=== Instagram Embedded Custom ===
+Contributors: teodoropiccinni
+Stable tag: 1.0.0
+Tested up to: 5.2
+Requires at least: 4.6
+
+This plugin will help you in removing some frustrating description and comments from embedded Instagram photos and videos.
+
+== Description ==
+
+This plugin will help you in removing some frustrating description and comments from embedded Instagram photos and videos.
